6965364d6433e8569150a9b23380cf43c69a7732
Blake Simpson <blake.simpson@xing.com>
2017-11-07 09:14:36 +0100

    Add a webpack config that will package the app into `dist`
    Contains a simple "app.jsx" that simply logs something - this on purpose
    uses ES6 string literals so that we see that running `yarn build` does
    not work - next we will add the Babel loader.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'app');
+const JS_DIR = path.resolve(APP_DIR, 'js');
+
+const config = {
+  entry: JS_DIR + '/app.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  }
+};
+
+module.exports = config;
