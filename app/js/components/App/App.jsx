862089ae2179977107b5c5336a369619e80b92e8
Blake Simpson <blake.simpson@xing.com>
2017-12-04 23:05:27 +0100

    Step 6: Adding the Map graphic to the app

--- app/js/components/App/App.jsx
+++ app/js/components/App/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Map from '../Map';
+
 const App = () => {
-  return <p>Rendered by React</p>;
+  return (
+    <section>
+      <Map />
+    </section>
+  );
 };
 
 export default App;
