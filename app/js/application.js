d6e34c8991b8678a8b3f1b61730f1639c2f222b5
Blake Simpson <blake.simpson@xing.com>
2017-12-04 21:56:45 +0100

    Step 5: Install React and create our first component

--- app/js/application.js
+++ app/js/application.js
@@ -1,1 +1,6 @@
-console.log(`it's working!`);
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './components/App';
+
+render(<App />, document.getElementById('app'));
