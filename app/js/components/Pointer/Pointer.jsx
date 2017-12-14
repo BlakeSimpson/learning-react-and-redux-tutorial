010bccef85039b068530d83484c2640e996a7be9
Blake Simpson <blake.simpson@kartenmacherei.de>
2017-12-14 15:58:14 +0100

    Step 10: Showing the Details of each Pointer

--- app/js/components/Pointer/Pointer.jsx
+++ app/js/components/Pointer/Pointer.jsx
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import styles from './Pointer.css';
 
 class Pointer extends Component {
-  render () {
-    const { x, y } = this.props;
+  constructor (props) {
+    super(props);
 
-    return <div className={styles.pointer} style={{ left: x, top: y }} />;
+    this.state = {
+      open: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle (event) {
+    event.preventDefault();
+
+    if (event.target === event.currentTarget) {
+      this.setState({ open: !this.state.open });
+    }
+  }
+
+  render () {
+    const { x, y, details } = this.props;
+    const { name, house, words } = details;
+    const detailsClasses = classNames(styles.details, {
+      [styles.hidden]: !this.state.open
+    });
+
+    return (
+      <div
+        className={styles.pointer}
+        style={{ left: x, top: y }}
+        onClick={this.toggle}
+      >
+        <div className={detailsClasses}>
+          <header className={styles.headline}>
+            <h3>{name}</h3>
+            <a href="#" className={styles.close} onClick={this.toggle}>
+              &times;
+            </a>
+          </header>
+
+          <p>House: {house}</p>
+          <p>Words: {words}</p>
+        </div>
+      </div>
+    );
   }
 }
 
 Pointer.propTypes = {
+  details: PropTypes.object.isRequired,
   x: PropTypes.number.isRequired,
   y: PropTypes.number.isRequired
 };
