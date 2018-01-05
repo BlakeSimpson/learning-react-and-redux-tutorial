e9d82949cb5c40e77c9210965a6804abee561ba3
Blake Simpson <blake.simpson@kartenmacherei.de>
2018-01-05 12:05:05 +0100

    Step 12: Favourites list

--- app/js/components/App/App.jsx
+++ app/js/components/App/App.jsx
@@ -1,14 +1,84 @@
 import React from 'react';
 
 import Map from '../Map';
+import FavouritesList from '../FavouritesList';
 
 // eslint-disable-next-line no-unused-vars
 import styles from './App.css';
 
+const points = [
+  {
+    x: 450,
+    y: 110,
+    details: {
+      name: 'The Wall',
+      house: "Night's Watch",
+      words: 'Night gathers, and now my watch begins.'
+    },
+    favourite: true
+  },
+  {
+    x: 375,
+    y: 355,
+    details: {
+      name: 'Winterfell',
+      house: 'Stark',
+      words: 'Winter is Coming'
+    },
+    favourite: true
+  },
+  {
+    x: 345,
+    y: 705,
+    details: {
+      name: 'The Twins',
+      house: 'Frey',
+      words: 'We Stand Together'
+    }
+  },
+  {
+    x: 155,
+    y: 775,
+    details: {
+      name: 'The Iron Islands',
+      house: 'Greyjoy',
+      words: 'We Do Not Sow'
+    }
+  },
+  {
+    x: 150,
+    y: 945,
+    details: {
+      name: 'Casterly Rock',
+      house: 'Lannister',
+      words: 'Hear me Roar!'
+    }
+  },
+  {
+    x: 545,
+    y: 1000,
+    details: {
+      name: "King's Landing",
+      house: 'Baratheon',
+      words: 'Ours is the Fury'
+    }
+  },
+  {
+    x: 250,
+    y: 1190,
+    details: {
+      name: 'Highgarden',
+      house: 'Tyrell',
+      words: 'Growing Strong'
+    }
+  }
+];
+
 const App = () => {
   return (
     <section>
-      <Map />
+      <Map points={points} />
+      <FavouritesList points={points} />
     </section>
   );
 };
--- app/js/components/Map/Map.jsx
+++ app/js/components/Map/Map.jsx
@@ -1,81 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Pointer from '../Pointer';
 import styles from './Map.css';
 
-const points = [
-  {
-    x: 450,
-    y: 110,
-    details: {
-      name: 'The Wall',
-      house: "Night's Watch",
-      words: 'Night gathers, and now my watch begins.'
-    },
-    favourite: true
-  },
-  {
-    x: 375,
-    y: 355,
-    details: {
-      name: 'Winterfell',
-      house: 'Stark',
-      words: 'Winter is Coming'
-    }
-  },
-  {
-    x: 345,
-    y: 705,
-    details: {
-      name: 'The Twins',
-      house: 'Frey',
-      words: 'We Stand Together'
-    }
-  },
-  {
-    x: 155,
-    y: 775,
-    details: {
-      name: 'The Iron Islands',
-      house: 'Greyjoy',
-      words: 'We Do Not Sow'
-    }
-  },
-  {
-    x: 150,
-    y: 945,
-    details: {
-      name: 'Casterly Rock',
-      house: 'Lannister',
-      words: 'Hear me Roar!'
-    }
-  },
-  {
-    x: 545,
-    y: 1000,
-    details: {
-      name: "King's Landing",
-      house: 'Baratheon',
-      words: 'Ours is the Fury'
-    }
-  },
-  {
-    x: 250,
-    y: 1190,
-    details: {
-      name: 'Highgarden',
-      house: 'Tyrell',
-      words: 'Growing Strong'
-    }
-  }
-];
-
-const Map = () => {
+const Map = ({ points }) => {
   return (
     <div className={styles.map}>
       {points.map((point, index) => <Pointer {...point} key={index} />)}
     </div>
   );
+};
+
+Map.propTypes = {
+  points: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default Map;
