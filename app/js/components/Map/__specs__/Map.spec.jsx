711ab6a768bc8fe318cba03daa7d97a1b4a78551
Blake Simpson <blake.simpson@kartenmacherei.de>
2018-01-12 11:59:03 +0100

    Step 22: Testing the ConnectedMap component

--- app/js/components/Map/__specs__/Map.spec.jsx
+++ app/js/components/Map/__specs__/Map.spec.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
 import { pointsMock } from '../../../spec-helper';
-import { Map } from '../';
+import ConnectedMap, { Map } from '../';
 
 describe('Map component', () => {
   describe('when there are no points', () => {
@@ -19,5 +20,16 @@ describe('Map component', () => {
 
       expect(wrapper).toMatchSnapshot();
     });
+  });
+});
+
+describe('ConnectedMap component', () => {
+  const mockStore = configureStore([]);
+  const store = mockStore({ points: pointsMock });
+
+  it('maps store state to the props', () => {
+    const wrapper = shallow(<ConnectedMap store={store} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
