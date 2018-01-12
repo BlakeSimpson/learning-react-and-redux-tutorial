156b4a72980742c776540d35bcef37148bf9f7a8
Blake Simpson <blake.simpson@kartenmacherei.de>
2018-01-12 12:35:26 +0100

    Step 24: Test the Favourites List

---
+++ app/js/components/FavouritesList/__specs__/FavouritesList.spec.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+
+import { pointsMock } from '../../../spec-helper';
+import { removeFavourite } from '../../../actions';
+import ConnectedFavouritesList, { FavouritesList } from '../';
+
+const noop = () => {};
+
+describe('FavouritesList component', () => {
+  describe('when there are no points', () => {
+    it('matches the snapshot', () => {
+      const wrapper = shallow(<FavouritesList points={[]} remove={noop} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('When there are points', () => {
+    it('matches the snapshot', () => {
+      const wrapper = shallow(
+        <FavouritesList points={pointsMock} remove={noop} />
+      );
+
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
+
+describe('ConnectedFavouritesList component', () => {
+  const mockStore = configureStore([]);
+  const store = mockStore({ points: pointsMock });
+
+  it('maps store state to the props', () => {
+    const wrapper = shallow(<ConnectedFavouritesList store={store} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('when the delete button is clicked', () => {
+    it('calls the removeFavourite action', () => {
+      const wrapper = mount(<ConnectedFavouritesList store={store} />);
+      const expectedAction = [removeFavourite('point-1')];
+
+      wrapper.find('button').simulate('click');
+
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
+});
