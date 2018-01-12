15478e47ff410301983256a05201c4e5f0c58b02
Blake Simpson <blake.simpson@kartenmacherei.de>
2018-01-12 11:17:29 +0100

    Step 20: Preparing Component testing with Jest

---
+++ app/js/spec-helper.js
@@ -0,0 +1,30 @@
+import 'raf/polyfill';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+export const pointsMock = [
+  {
+    id: 'point-1',
+    x: 450,
+    y: 110,
+    details: {
+      name: 'The Wall',
+      house: "Night's Watch",
+      words: 'Night gathers, and now my watch begins.'
+    },
+    favourite: true
+  },
+  {
+    id: 'point-2',
+    x: 375,
+    y: 355,
+    details: {
+      name: 'Winterfell',
+      house: 'Stark',
+      words: 'Winter is Coming'
+    },
+    favourite: false
+  }
+];
